a1d5b044da800284b5f40b9345d106810be42b2b
Julien Alardot <alardotj.pro@gmail.com>
2021-05-03 10:59:39 +0200

    Adding necessary Data infos

---
+++ Core/datautils.py
@@ -0,0 +1,76 @@
+import pandas as pd
+
+
+class DataBase:
+    COLUMNS = (
+        "id",
+        "url",
+        "source",
+        "street",
+        "number",
+        "zip",
+        "locality",
+        "property_type",
+        "property_subtype",
+        "price",
+        "sale_type",
+        "rooms",
+        "inside_area",
+        "full_equ_kitchen",
+        "furnished",
+        "open_fire",
+        "terrace",
+        "terrace_area",
+        "garden",
+        "garden_area",
+        "land_area",
+        "facades",
+        "swmming_pool",
+        "building_state"
+    )
+
+    DTYPES = (
+        int,
+        str,
+        str,
+        str,
+        str,
+        int,
+        str,
+        str,
+        str,
+        int,
+        str,
+        int,
+        int,
+        bool,
+        bool,
+        bool,
+        bool,
+        int,
+        bool,
+        int,
+        int,
+        int,
+        bool,
+        str
+    )
+
+    COLUMNSDATA = {c: d for c, d in zip(COLUMNS, DTYPES)}
+
+    __DATATEMPLATE = pd.DataFrame([], columns=COLUMNS)
+
+    for column in __DATATEMPLATE.columns:
+        __DATATEMPLATE[column] = __DATATEMPLATE[column].astype(COLUMNSDATA[column])
+
+    DATAFRAMETEMPLATE = __DATATEMPLATE.copy()
+
+
+if __name__ == "__main__":
+    print(DataBase.COLUMNS)
+    print(DataBase.DTYPES)
+
+    for k, v in DataBase.COLUMNSDATA.items():
+        print((k, v))
+    print(DataBase.DATAFRAMETEMPLATE.info())
+
