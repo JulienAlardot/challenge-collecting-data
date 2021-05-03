6b2cfc54ee983c63ca9d33b6e71ded954a0edf19
AlainTiri <alaintiri@gmail.com>
2021-05-03 17:37:38 +0200

    immoweb.py: 1er generator des listes d'url paré à fonctionner.

--- PerSiteApi/immoweb.py
+++ PerSiteApi/immoweb.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3.8
 
 import re
 from typing import List, Dict
+from Core.datautils import DataStruct
+import pandas as pd
 
 
 class Immoweb():
@@ -10,6 +11,7 @@ class Immoweb():
         self.data_immo: Dict = {}
         self.list_URL: List[str] = []
         self.url_error: List[str] = []
+        self.zip_code: List = DataStruct().get_zipcode_data()
 
     def _ajouteURL(self, url, valeur=False):
         # vérifie si une url est dans le fichier
@@ -31,34 +33,42 @@ class Immoweb():
         return {}
 
     def run(self):
+        #  print (self.zip_code.zipcode)
         self._generator_db_url()
 
-    def _scan_page_list(self, url):
+    def _scan_page_list(self, url: str) -> bool:
         print(url)
 
     def _generator_db_url(self) -> bool:
-        """https://www.immoweb.be/fr/recherche/maison-et-appartement/a-vendre?countries=BE&page=3&orderBy=relevance"""
-        """https://www.immoweb.be/fr/recherche/maison-et-appartement/a-louer?countries=BE&minPrice=700&maxPrice=1000&page=315&orderBy=cheapest"""
+        """https://www.immoweb.be/fr/recherche/maison-et-appartement/a-vendre?countries=BE&page=3&orderBy=relevance
+        https://www.immoweb.be/fr/recherche/maison-et-appartement/a-louer?countries=BE&minPrice=700&maxPrice=1000&page=315&orderBy=cheapest
+        https://www.immoweb.be/fr/recherche/maison-et-appartement/a-vendre?countries=BE&postalCodes=BE-1341,BE-1650&orderBy=cheapest"""
 
         init_url = "https://www.immoweb.be/fr/recherche/maison-et-appartement/"
-        type_of = ['a-vendre',
+        middle_url = [
                    'a-louer?countries=BE&minPrice=0&maxPrice=700&page=',
                    'a-louer?countries=BE&minPrice=700&maxPrice=1099&page=',
                    'a-louer?countries=BE&minPrice=1100&maxPrice=1499&page=',
                    'a-louer?countries=BE&minPrice=1500page='
                    ]
-        renting = [0, 699, 1099, 1499, 1500]
-        buy = [0, 178999, 22800, 266000]
+
         middle_url = '?countries=BE&'
         end_url = '&orderBy=cheapest'
         num_max_page = 333
 
-        for type_in in type_of:
-            for pager in range(0, num_max_page + 1):
+        for type_in in middle_url:
+            for pager in range(1, num_max_page + 1):
                 url_list = f"{init_url}{type_in}{pager}{end_url}"
 
             self._scan_page_list(url_list)
+
+        url_vente = "https://www.immoweb.be/fr/recherche/maison-et-appartement/a-vendre?countries=BE&postalCodes=BE-"
+        for zip in self.zip_code.zipcode:
+            url_list = f"{url_vente}{zip}{end_url}"
+            self._scan_page_list(url_list)
+
         return True
+
 
 
 immoweb = Immoweb()
