0343bfe848dfb4ed89f9fe67aa3c5d15c349a7eb
Julien Alardot <alardotj.pro@gmail.com>
2021-05-04 23:44:09 +0200

    Update LogicImmo.py

--- PerSiteApi/LogicImmo.py
+++ PerSiteApi/LogicImmo.py
@@ -19,20 +19,21 @@ __logic_immo_url = os.path.join(__data_path, "logic_immo_url.csv")
 __logic_immo_raw_data = os.path.join(__data_path, "logic_immo_raw_data.csv")
 __root_url = r"https://www.logic-immo.be"
 __addresses = set()
-__pattern = re.compile(r'/fr/vente/.+/[a-zA-Z]+-[0-9]+/.+.html')
+__pattern = re.compile(r'/fr/vente/.+/.+/.+\.html')
 
 __zipcode_df = pd.read_csv(__zipcode_file)
 __headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding": "gzip, deflate",
-    "Accept-Language": "fr-FR,en-US;q=0.9,en;q=0.8",
-    "Dnt": "1",
-    "Host": "httpbin.org",
-    "Upgrade-Insecure-Requests": "1",
+    # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    # "Accept-Encoding": "gzip, deflate",
+    # "Accept-Language": "fr-FR,en-US;q=0.9,en;q=0.8",
+    # "Dnt": "1",
+    # "Host": "httpbin.org",
+    # "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
-    "X-Amzn-Trace-Id": "Root=1-5ee7bae0-82260c065baf5ad7f0b3a3e3"
+    # "X-Amzn-Trace-Id": "Root=1-5ee7bae0-82260c065baf5ad7f0b3a3e3"
 }
 def search_for_urls():
+    old_len_address = 0
     for i, row in __zipcode_df.iterrows():
         print(i)
         print(row["local"])
@@ -43,6 +44,10 @@ def search_for_urls():
             found = False
             url = __root_url + f'/fr/vente/immo-a-vendre/{text}-{row["zipcode"]},{page},--------16776966-,---,---.html'
             response = requests.get(url, headers=__headers)
+            if response.status_code == 500 or response.status_code == 503:
+                while response.status_code == 500 or response.status_code == 503:
+                    response = requests.get(url)
+                    time.sleep(1)
             buy_soup = bs4.BeautifulSoup(response.content, features="html.parser")
             for link in (f.get("href") for f in buy_soup.findAll("a")):
                 if link:
@@ -52,10 +57,16 @@ def search_for_urls():
 
             if not found:
                 text = row["local"].lower().replace(" ", "-").replace("é", "e").replace("è", "e").replace("â", "a")
-                text = text.replace("ç", "c").replace("û", "u").replace("ê", "e").replace("î", "i")
+                text = text.replace("ç", "c").replace("û", "u").replace("ê", "e").replace("î", "i").replace("'", "")
+                text = text.replace("ô", "o")
                 url = __root_url + f'/fr/vente/immo-a-vendre/{text}-{row["zipcode"]},{page},--------16776966-,---,---.html'
 
                 response = requests.get(url)
+
+                if response.status_code == 500 or response.status_code == 503:
+                    while response.status_code == 500 or response.status_code == 503:
+                        response = requests.get(url)
+                        time.sleep(1)
                 buy_soup = bs4.BeautifulSoup(response.content, features="html.parser")
 
                 for link in (f.get("href") for f in buy_soup.findAll("a")):
@@ -68,7 +79,8 @@ def search_for_urls():
             # time.sleep(random.random()/4)
             page += 1
 
-            if len(__addresses) % 480 == 0:
+            if len(__addresses) > old_len_address + 1000:
+                old_len_address = len(__addresses)
                 with open(__logic_immo_url, 'wt+', newline='', encoding="utf-8") as url_file:
                     f_w = csv.writer(url_file)
                     f_w.writerows(([address] for address in sorted(__addresses)))
@@ -103,5 +115,5 @@ def search_raw_infos():
 
 
 if __name__ == "__main__":
-    # search_for_urls()
-    search_raw_infos()
+    search_for_urls()
+    # search_raw_infos()
